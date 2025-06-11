2fd4f2ad1693eebccf481d911954778bfacec430
unknown <sgirraj66@gmail.com>
2025-06-11 16:37:54 +0530

    soft at nokia

--- src/Components/UserInterface/SOFT AT/NokiaStatus/AddCheckListData.js
+++ src/Components/UserInterface/SOFT AT/NokiaStatus/AddCheckListData.js
@@ -52,7 +52,7 @@ const AddCheckListData = (props) => {
             var formData = new FormData();
             // console.log('pre files' , preFiles[i])
             formData.append(`excel_file`, make4GFiles.bytes);
-            const response = await postData(`soft_at_nokia/${props.api}/`, formData)
+            const response = await postData(`Soft_AT_Checklist_Nokia/${props.api}/`, formData)
             // console.log('response data', response)
             if (response) {
                 action(false)
