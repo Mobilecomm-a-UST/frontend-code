1975f15c6bfe74d4094a4cd13dbf01e3b807d64b
unknown <sgirraj66@gmail.com>
2025-06-13 15:09:55 +0530

    add correction in ix tracker in react

--- src/Components/services/FetchNodeServices.js
+++ src/Components/services/FetchNodeServices.js
@@ -3,14 +3,14 @@ import { getDecreyptedData } from "../utils/localstorage";
 
 
 // const ServerURL = "http://192.168.0.182:8000"
-const ServerURL = "http://127.0.0.1:8000"
+// const ServerURL = "http://127.0.0.1:8000"
 // const ServerURL = "http://192.168.1.16:8001"
 // const ServerURL = "http://192.168.0.146:8000"
 // const ServerURL = "http://103.242.225.195:8000"
 // const ServerURL = "http://192.168.0.44:8001"
 // const ServerURL = "http://192.168.0.10:8000"
 // const ServerURL = "http://103.242.225.195:8000";    
-// const ServerURL = "http://122.176.141.197:8000";    
+const ServerURL = "http://122.176.141.197:8000";    
 // const ServerURL = "http://192.168.1.7:8000"
 // const ServerURL = "http://54.234.191.244"
 // const ServerURL = "http://13.233.231.179:80"
