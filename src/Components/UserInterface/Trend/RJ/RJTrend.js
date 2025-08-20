bd9cc20b7751c0655982cf68b39d5faefae13621
Girraj Singh <85035083+Girraj-singh@users.noreply.github.com>
2025-08-20 11:24:30 +0530

    new update in RAJ trend tool

--- src/Components/UserInterface/Trend/RJ/RJTrend.js
+++ src/Components/UserInterface/Trend/RJ/RJTrend.js
@@ -1,74 +1,85 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
-import {Grid} from '@mui/material'
-import { Sidenav, Nav} from 'rsuite';
+import { Grid } from '@mui/material'
+import { Sidenav, Nav } from 'rsuite';
 import FileUploadIcon from '@rsuite/icons/FileUpload';
 import { useNavigate } from 'react-router-dom'
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MakeKPITrendOld from './Make_Trend(old)/MakeKPITrendOld'
 import TrendBox from '../TrendBox';
+import KpiTrend2G from './Make_Trend(old)/KpiTrend2G';
+import KpiTrend4G from './Make_Trend(old)/KpiTrend4G';
+
 
 
 function RJTrend() {
-    const [expanded, setExpanded] = useState(true);
-    const [activeKey, setActiveKey] = useState('1');
-    const [states,setStates] =  useState([])
+  const [expanded, setExpanded] = useState(true);
+  const [activeKey, setActiveKey] = useState('1');
+  const [states, setStates] = useState([])
 
-    const navigate=useNavigate()
-    const headData = document.location.pathname.split('/');
-    useEffect(()=>{
-      document.title=`${window.location.pathname.slice(1).replaceAll('_', ' ').replaceAll('/',' | ').toUpperCase()}`
-    },[])
+  const navigate = useNavigate()
+  const headData = document.location.pathname.split('/');
+  useEffect(() => {
+    document.title = `${window.location.pathname.slice(1).replaceAll('_', ' ').replaceAll('/', ' | ').toUpperCase()}`
+  }, [])
 
 
   return (
     <div >
 
-        <Grid container spacing={1}>
-            <Grid item xs={2}>
-            <div >
-      <Sidenav expanded={expanded} defaultOpenKeys={['1','3']} appearance="subtle">
-        <Sidenav.Body>
-          <Nav activeKey={activeKey} onSelect={setActiveKey} style={{ width:'',minHeight:"670px",height:"100hv",backgroundColor:"#223354",marginTop:8,borderRadius:10,position:'fixed' }}>
-          <Nav style={{fontWeight:600,color:'white',textAlign:'center',fontSize:20}}>RJ Trend</Nav>
+      <Grid container spacing={1}>
+        <Grid item xs={2}>
+          <div >
+            <Sidenav  expanded={expanded} defaultOpenKeys={[]} appearance="subtle" style={{ minHeight: "670px", height: "100vh", backgroundColor: "#223354", marginTop: 8, borderRadius: 10 }}>
+              <Sidenav.Body>
+                <Nav activeKey={activeKey} onSelect={setActiveKey}>
+                  <Nav style={{ fontWeight: 600, color: 'white', textAlign: 'center', fontSize: 20 }}>RAJ/NESA Trend</Nav>
 
-            {/* <Nav.Menu placement="rightStart" eventKey="3" title="Pre-Post Upload" icon={<FileUploadIcon/>}>
+                  {/* <Nav.Menu placement="rightStart" eventKey="3" title="Pre-Post Upload" icon={<FileUploadIcon/>}>
 
               <Nav.Item eventKey="3-2"
                onClick={()=>navigate(-1)}
                >Pre-Post Report</Nav.Item>
 
             </Nav.Menu> */}
-            {/* <Nav.Item eventKey="4" icon={<FileUploadIcon/>} onClick={()=>navigate(-1)}>
+                  {/* <Nav.Item eventKey="4" icon={<FileUploadIcon/>} onClick={()=>navigate(-1)}>
                Make Trend
             </Nav.Item> */}
-            <Nav.Item eventKey="5" icon={<FileUploadIcon/>} onClick={()=>navigate('/trends/rj/make_kpi_trend_old')}>
-               Make Trend(old)
-            </Nav.Item>
+                  <Nav.Menu activeKey="2" icon={<FileUploadIcon />} title="Make Trend">
+                    <Nav.Item eventKey="2-1" onClick={() => navigate('/trends/rj/kpi_trend_2G')}>
+                      Trend 2G
+                    </Nav.Item>
+                    <Nav.Item eventKey="2-2" onClick={() => navigate('/trends/rj/kpi_trend_4G')} >
+                      Trend 4G
+                    </Nav.Item>
 
-          </Nav>
-        </Sidenav.Body>
+                  </Nav.Menu>
 
-      </Sidenav>
-    </div>
-            </Grid>
-            <Grid item xs={10}>
-            <div >
+                </Nav>
+              </Sidenav.Body>
 
-
-
-        <Routes>
-
-
-          <Route element={<TrendBox data={'RJ'} />} path="/" />
-          <Route element={<MakeKPITrendOld/>} path="make_kpi_trend_old"/>
-
-        </Routes>
-
-    </div>
-
-            </Grid>
+            </Sidenav>
+          </div>
         </Grid>
+        <Grid item xs={10}>
+          <div >
+
+
+
+            <Routes>
+
+
+              <Route element={<TrendBox data={'RAJ / NESA'} />} path="/" />
+              {/* <Route element={<MakeKPITrendOld/>} path="make_kpi_trend_old"/> */}
+              <Route element={<KpiTrend2G />} path="kpi_trend_2G" />
+              <Route element={<KpiTrend4G />} path="kpi_trend_4G" />
+
+            </Routes>
+
+          </div>
+
+        </Grid>
+      </Grid>
     </div>
   )
 }
