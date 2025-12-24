20ecd4c038c9a3268a3698d1e51560b7b507fce0
Girraj Singh <85035083+Girraj-singh@users.noreply.github.com>
2025-12-24 23:23:43 +0530

    add one more ix tracker

--- src/Components/UserInterface/Integration_Tools/Ix_Tools.js
+++ src/Components/UserInterface/Integration_Tools/Ix_Tools.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import { Box, Grid } from '@mui/material'
 import { useStyles } from '../ToolsCss'
 import { useNavigate } from 'react-router-dom'
@@ -10,56 +10,56 @@ import TimeIcon from '@rsuite/icons/Time';
 import { getDecreyptedData } from '../../utils/localstorage'
 
 const Ix_Tools = () => {
-        const dispatch = useDispatch()
-        const classes = useStyles()
-        const navigate = useNavigate()
-        const chackToken = getDecreyptedData("tokenKey")
-        const userTypes = (getDecreyptedData('user_type')?.split(","))
-        const allowedAirtelRoles = ['admin', 'IX','IX_reader','soft_at_team'];
-        const allowedViRoles = ['admin', 'VI_IX','VI_IX_reader','soft_at_team'];
-    
-        // console.log('ggggggggg', window.location.href)
-        const linker = window.location.pathname;
+    const dispatch = useDispatch()
+    const classes = useStyles()
+    const navigate = useNavigate()
+    const chackToken = getDecreyptedData("tokenKey")
+    const userTypes = (getDecreyptedData('user_type')?.split(","))
+    const allowedAirtelRoles = ['Admin', 'IX', 'IX_reader', 'soft_at_team'];
+    const allowedViRoles = ['Admin', 'VI_IX', 'VI_IX_reader', 'soft_at_team'];
+
+    console.log('ggggggggg', userTypes)
+    const linker = window.location.pathname;
 
 
-        const handleAirtel = () => {
-            if (chackToken == null) {
-                navigate('/login')
-                dispatch({ type: 'LINK_PAGES', payload: { linker } })
-            }
-            else {
-                navigate('/tools/Integration')
-            }
+    const handleAirtel = () => {
+        if (chackToken == null) {
+            navigate('/login')
+            dispatch({ type: 'LINK_PAGES', payload: { linker } })
         }
-        const handleVi = () => {
-            if (chackToken == null) {
-                navigate('/login')
-                dispatch({ type: 'LINK_PAGES', payload: { linker } })
-            }
-            else {
-                navigate('/tools/ix_tools/vi_integration/')
-            }
+        else {
+            navigate('/tools/Integration')
         }
+    }
+    const handleVi = () => {
+        if (chackToken == null) {
+            navigate('/login')
+            dispatch({ type: 'LINK_PAGES', payload: { linker } })
+        }
+        else {
+            navigate('/tools/ix_tools/vi_integration/')
+        }
+    }
 
 
-        const backgroundStyle = {
-            height: "auto",
-            width: "300px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            borderRadius: 3,
-            boxShadow: "-10px -10px 15px rgba(255,255,255,0.4),10px 10px 15px rgba(70,70,70,0.15)",
-            textShadow: '2px 2px 4px #ffffff',
-            color: '#223354',
-            // border:"1px solid black"
-        }
-    
-        useEffect(()=>{
-            document.title=`${window.location.pathname.slice(1).replaceAll('_', ' ').replaceAll('/',' | ').toUpperCase()}`
-    
-        },[])
-  return (
-    <div style={{ backgroundColor: '#FBEEE6' }}>
+    const backgroundStyle = {
+        height: "auto",
+        width: "300px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        borderRadius: 3,
+        boxShadow: "-10px -10px 15px rgba(255,255,255,0.4),10px 10px 15px rgba(70,70,70,0.15)",
+        textShadow: '2px 2px 4px #ffffff',
+        color: '#223354',
+        // border:"1px solid black"
+    }
+
+    useEffect(() => {
+        document.title = `${window.location.pathname.slice(1).replaceAll('_', ' ').replaceAll('/', ' | ').toUpperCase()}`
+
+    }, [])
+    return (
+        <div style={{ backgroundColor: '#FBEEE6' }}>
 
             <Box style={{ padding: "15px", marginTop: '60px' }}>
                 <Box sx={{ display: "flex", justifyContent: 'center' }}>
@@ -91,20 +91,20 @@ const Ix_Tools = () => {
                         </Box> */}
                         <Box sx={{ width: '90%', marginLeft: 'auto', marginRight: 'auto' }}>
                             <Grid container rowSpacing={2} columnSpacing={1} direction={{ xs: "column", sm: "column", md: "row" }} style={{}}>
-                                {userTypes?.some(role => allowedAirtelRoles.includes(role)) && <Grid item xs={4}>
-                                    <Box sx={backgroundStyle} className={classes.des} onClick={handleAirtel}>
-                                        <div className={classes.centerIcon}>   <img src="/assets/AIRTEL.webp" alt="Airtel" style={{ width: "70px", height: "70px" }} /></div>
-                                        <div >
-                                            <div className={classes.center} >Airtel Tracker</div>
-                                            {/* <div>This is a FDD tool</div> */}
-                                        </div>
-                                        {/* <span className={classes.center}>FILE MERGE</span> */}
-                                    </Box>
-                                </Grid>}
-                             
-                                 {userTypes?.some(role => allowedViRoles.includes(role)) && <Grid item xs={4}>
+                                {userTypes?.some(role =>allowedAirtelRoles.map(r => r.toLowerCase()).includes(role?.toLowerCase())) && <Grid item xs={4}>
+                                        <Box sx={backgroundStyle} className={classes.des} onClick={handleAirtel}>
+                                            <div className={classes.centerIcon}>   <img src="/assets/AIRTEL.webp" alt="Airtel" style={{ width: "60px", height: "60px" }} /></div>
+                                            <div >
+                                                <div className={classes.center} >Airtel Tracker</div>
+                                                {/* <div>This is a FDD tool</div> */}
+                                            </div>
+                                            {/* <span className={classes.center}>FILE MERGE</span> */}
+                                        </Box>
+                                    </Grid>}
+
+                                {userTypes?.some(role =>allowedViRoles.map(r => r.toLowerCase()).includes(role?.toLowerCase())) &&<Grid item xs={4}>
                                     <Box sx={backgroundStyle} className={classes.des} onClick={handleVi}>
-                                        <div className={classes.centerIcon}> <img src="/assets/VI.webp" alt="VI" style={{ width: "70px", height: "70px" }} /></div>
+                                        <div className={classes.centerIcon}> <img src="/assets/VI.webp" alt="VI" style={{ width: "60px", height: "60px" }} /></div>
                                         <div >
                                             <div className={classes.center}>VI Tracker</div>
                                             {/* <div>This is a TDD tool</div> */}
@@ -119,7 +119,7 @@ const Ix_Tools = () => {
                 </Zoom>
             </Box>
         </div>
-  )
+    )
 }
 
 export default Ix_Tools
